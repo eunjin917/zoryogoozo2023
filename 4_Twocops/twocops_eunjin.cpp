6d7de31173725a1f489255c28b1686f98645757d
eunGnee <64008627+eunjin917@users.noreply.github.com>
2023-09-22 02:39:22 +0900

    Create twocops_eunjin.cpp

---
+++ 4_Twocops/twocops_eunjin.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio  \
+    iostream::sync_with_stdio(false);   \
+    cin.tie(nullptr);   \
+    cout.tie(nullptr);
+
+struct point{
+    int x;
+    int y;
+    char direction;   // up down right left
+    int time;
+};
+
+vector<point> Points;
+int N, Cycle_time=0, Upper_time, Lower_time, Robot_time, C2_time;
+
+void calc(int last_x, int last_y, int this_x, int this_y) {
+    int diff;
+    char direction;
+
+    if (this_x == last_x) {
+        diff = this_y - last_y;
+        direction = (diff > 0) ? 'u' : 'd';
+    }
+    else if (this_y == last_y) {
+        diff = this_x - last_x;
+        direction = (diff > 0) ? 'r': 'l';
+    }
+
+    Points.push_back({last_x, last_y, direction, Cycle_time});
+
+    Cycle_time += (diff > 0) ? diff : -diff;
+}
+
+void set_points() {
+    int last_x, last_y;
+    cin >> N >> last_x >> last_y;
+
+    // 0 ~ N-2
+    for (int i=0; i<N-1; i++) {
+        int this_x, this_y;
+        cin >> this_x >> this_y;
+
+        calc(last_x, last_y, this_x, this_y);
+
+        last_x = this_x;
+        last_y = this_y;
+    }
+
+    // N-1
+    calc(last_x, last_y, Points[0].x, Points[0].y);
+
+
+    // view
+
+
+    /*
+    for (int i=0; i<N; i++) {
+        cout << Points[i].x << " " << Points[i].y << " " << Points[i].direction <<" " << Points[i].time << "\n";
+    }
+    */
+
+
+    C2_time = Points[N/2-1].time;
+    Upper_time = C2_time / 2;
+    Lower_time = C2_time + (Cycle_time - C2_time)/2;
+    Robot_time = Cycle_time;
+}
+
+void output(int time, int i) {
+
+                int diff = time - Points[i].time;
+
+                switch (Points[i].direction) {
+                case 'u':
+                    cout << Points[i].x << " " << Points[i].y +diff << "\n";
+                    break;
+                case 'd':
+                    cout << Points[i].x << " " << Points[i].y -diff << "\n";
+                    break;
+                case 'r':
+                    cout << Points[i].x +diff << " " << Points[i].y << "\n";
+                    break;
+                case 'l':
+                    cout << Points[i].x -diff << " " << Points[i].y << "\n";
+                    break;
+                }
+}
+
+
+void find_time(int time) {
+        int index;
+        for (index=1; index<N; index++) {   // 1~ N-1
+            if (time < Points[index].time) {  // range! 1400 -> last
+                output(time, index-1);
+                break;
+            }
+
+        }
+        if (index==N && time < Cycle_time) {
+            output(time, index-1);
+        }
+}
+
+void testcase() {
+    int seconds;
+    cin >> seconds;
+
+    int time = seconds % Robot_time;
+
+    if (time <= Upper_time) {
+        find_time(time);
+        find_time(C2_time - time);
+    }
+    else if (time <= C2_time) {
+        find_time(C2_time - time);
+        find_time(time);
+    }
+    else if (time <= Lower_time) {
+        find_time(C2_time - time + Robot_time);
+        find_time(time);
+    }
+    else if (time < Robot_time) {
+        find_time(time);
+        find_time(Robot_time - time + C2_time);
+    }
+}
+
+
+int main() {
+    set_points();
+    testcase();
+}
